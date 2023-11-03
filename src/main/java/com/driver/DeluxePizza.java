d6d1045700691c3ef586c099307df11d84107028
Soham <sohamjpathak@gmail.com>
2023-11-03 16:26:27 +0530

    second commit

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,17 +3,11 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
     //private boolean takeaway;
-    int price;
-    String bill;
-    public DeluxePizza(Boolean isVeg,Boolean takeaway) {
+
+    public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes her
             addExtraCheese();
             addExtraToppings();
-            if(takeaway)
-                addTakeaway();
-
-            this.bill=getBill();
-            this.price=getPrice();
     }
 }
